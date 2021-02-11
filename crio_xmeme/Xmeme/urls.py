8c406a7864a8d546ff021a5861f9b05389ab9bca
Shubham Kumar <theshubhamkumar01@gmail.com>
2021-02-11 10:25:55 +0530

    added json route

--- crio_xmeme/Xmeme/urls.py
+++ crio_xmeme/Xmeme/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('memes-update/<int:id>/', views.memes_form, name='memes_update'),
 
     # get req. to retrieve and display all records.
-    path('memes/', views.memes_List, name='memes_List'), 
+    path('', views.memes_List, name='memes_List'), 
 
-    # to get the desired id field.
+    # to get JSON of the all id field.
+    path('memes/', views.memes_json_all, name='meme_json_all'), 
+
+    # to get JSON of the desired id field.
     path('memes/<int:id>/', views.memes_json, name='meme_json'),    
     
     # post req. to delete a meme using it's id
